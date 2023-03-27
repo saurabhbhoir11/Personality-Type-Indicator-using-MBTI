f4cd4929000a9891b03a3040625392f7f8066441
Suyash <suyashbagwe717@gmail.com>
2023-03-27 21:08:59 +0530

    tweets extraction using snscrape

---
+++ tweets.py
@@ -0,0 +1,22 @@
+import snscrape.modules.twitter as sntwitter
+import pandas as pd
+
+query = "pandas"
+tweets = []
+limit = 5000
+
+
+for tweet in sntwitter.TwitterSearchScraper(query).get_items():
+    
+    # print(vars(tweet))
+    # break
+    if len(tweets) == limit:
+        break
+    else:
+        tweets.append([tweet.date, tweet.user.username, tweet.content])
+        
+df = pd.DataFrame(tweets, columns=['Date', 'User', 'Tweet'])
+print(df)
+
+# to save to csv
+# df.to_csv('tweets.csv')
